52dc4283e539157fd687d24ba92dcdc9189c9b65
yelis <110356354+yelis-alt@users.noreply.github.com>
2023-01-20 14:18:25 +0300

    20.01.23

---
+++ yandex_map_marks/description.md
@@ -0,0 +1,4 @@
+Разработка скрипта на языке программирования Python, раскрашивающего метки добавленных электронных зарядных станций на карте с определённой позиции в "Конструкторе карт" от Яндекса.
+</br> 
+</br> 
+The development of the script with the programming language Python that paints labels of added electronic charging stations on map from a specific position in "Map constructor" from Yandex.
---
+++ yandex_map_marks/yandex_map_label.py
@@ -0,0 +1,76 @@
+#Библиотеки
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import pyperclip
+import pyautogui
+import time
+import os
+
+class web_rpa():
+    #Функция ожидания появления элемента
+    def waitForLoad(self, inputXPath):
+        Wait = WebDriverWait(driver, 60)
+        Wait.until(EC.element_to_be_clickable((By.XPATH, inputXPath)))
+    #Функция проверки существования элемента
+    def checkExistence(self, inputXPath):
+        try:
+            check = driver.find_element('xpath', inputXPath)
+        except:
+            return False
+        return True
+
+#Формирование драйвера
+options = webdriver.ChromeOptions()
+codec = Service('../yandexdriver.exe')
+driver = webdriver.Chrome(service = codec, options=options)
+
+#Подготовка к действиям
+start = 83 ##начальная метка закраски
+url = 'https://yandex.ru/map-constructor'
+path = os.path.abspath("./zas.xlsx").replace('\x5c', '//')
+driver.get(url)
+time.sleep(2)
+
+#Открытие конструктора
+opens = driver.find_element('xpath', '//span[@class="button__text" and text()="Открыть карту"]')
+opens.click()
+waited_element = '//span[@class="button__text" and text()="К импорту →"]'
+web_rpa().waitForLoad(waited_element)
+
+#Загрузка файла с координатами
+xpaths = [waited_element,
+          '//input[@title="Выбрать файл"]']
+for i in range(len(xpaths)):
+    element = driver.find_element('xpath', xpaths[i])
+    element.click()
+pyperclip.copy(path)
+pyperclip.paste()
+pyautogui.press('enter')
+time.sleep(3)
+
+#Перекрашивание добавленных точек
+ids = f'//div[contains(@data-id, "_{start}")]'
+while web_rpa().checkExistence(ids):
+    xpaths = [ids,
+              '(//span[@class="icon select__tick"])[4]',
+              '//div[@class="menu__item menu__item_theme_colors menu__item_index_7 i-bem menu__item_js_inited"]']
+    for i in range(len(xpaths)):
+        element = driver.find_element('xpath', xpaths[i])
+        element.click()
+    start += 1
+    ids = f'//div[contains(@data-id, "_{start}")]'
+
+#Сохранение результата:
+xpaths = ['//span[@class="button__text" and text()="Сохранить и продолжить"]',
+          '//a[text()="Экспорт"]',
+          '//span[@class="button__text" and text()="KML"]',
+          '//span[@class="button__text" and text()="Скачать"]']
+for i in range(len(xpaths)):
+    time.sleep(2)
+    element = driver.find_element('xpath', xpaths[i])
+    element.click()
+time.sleep(5)
+driver.close()
