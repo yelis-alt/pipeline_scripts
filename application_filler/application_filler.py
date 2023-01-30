656d2346a5ce69ecf6fb81467645e9afc1dc3822
yelis <110356354+yelis-alt@users.noreply.github.com>
2023-01-30 13:00:56 +0300

    30.01.23

--- application_filler/application_filler.py
+++ application_filler/application_filler.py
@@ -1,15 +1,31 @@
 #Импорт библиотек
 from transliterate import translit
 import win32com.client
-#import random
+import pyautogui
+import pyperclip
+import time
+import random
 
 class text_inst():
-    #Функция поиска первого числа в строке
-    def number_pos(self, string):
+    #Метод поиска первого числа в строке
+    def number_pos(self, string ):
         for i, c in enumerate(string):
             if c.isdigit():
                 return i
                 break
+    #Метод вставки текста в поле
+    def field_paste(self, field):
+        pyperclip.copy(field)
+        pyautogui.hotkey('ctrl', 'v')
+text = text_inst()
+
+class key_inst():
+    #Метод повторения нажатия клавиш
+    def cycle(self, count, key):
+        for i in range(count):
+            pyautogui.press(key)
+key = key_inst()
+
 
 #Выделение строчек из письма
 strings = []
@@ -18,10 +34,11 @@ mapi = outlook.GetNamespace("MAPI")
 inbox = mapi.GetDefaultFolder(6)
 messages = inbox.Items
 messages.Sort("[ReceivedTime]", True)
+subject = 'В ответственность Вашей команды назначена заявка'
 for message in messages:
     if (message.UnRead) and \
-    ((message.subject.startswith("FW: В ответственность Вашей команды назначена заявка")) or \
-    (message.subject.startswith("В ответственность Вашей команды назначена заявка"))) and \
+    ((message.subject.startswith('FW: ' + subject)) or \
+    (message.subject.startswith(subject))) and \
     (message.body.find('ПРИЕМ НА РАБОТУ') > -1):
         string_pos_start = message.body.find('ФИО')
         string = message.body[string_pos_start::]
@@ -31,21 +48,74 @@ for message in messages:
 
 #Формирование текстовых полей для заполнения
 symbols = ['!', '#', '$', '%', '+', '&', '@']
-for string in strings:
+for k, string in enumerate(strings):
     string =  string[50:]
-    date_start = text_inst().number_pos(string)
+    date_start = text.number_pos(string)
     fio = string[:date_start]
     surname, name, patronymic = fio.split()
-    birth = string[len(string)-10::]
 
     #Формирование логина
     login_base = translit(surname, 'ru',  reversed=True).lower()
     login_init1 =  translit(name[0], 'ru',  reversed=True).lower()
     login_init2 =  translit(patronymic[0], 'ru',  reversed=True).lower()
     login = login_base + '-' + login_init1 + login_init2
+    login = login.replace("'", '')
 
     #Формирование пароля
-    #random_num = random.randint(1000,9999)
-    #random_symbol = random.choice(symbols)
-    #password = login_base.capitalize() + str(random_num) + random_symbol
-    password = 'Zaq123456&'
+    random_num = random.randint(1000,9999)
+    random_symbol = random.choice(symbols)
+    password = login_base.capitalize() + str(random_num) + random_symbol
+
+    #Активация окна активной директории
+    window = pyautogui.getWindowsWithTitle("Active Directory - пользователи и компьютеры")
+    window[0].activate()
+
+    #Открытие формы
+    if k == 0:
+        key.cycle(10, 'left')
+        pyautogui.press('right')
+        key.cycle(2, 'down')
+        pyautogui.press('right')
+        key.cycle(17, 'down')
+        pyautogui.press('right')
+        key.cycle(9, 'down')
+        pyautogui.hotkey('shift', 'f10')
+        key.cycle(4, 'down')
+        key.cycle(2, 'enter')
+    else:
+        pyautogui.hotkey('shift', 'f10')
+        key.cycle(4, 'down')
+        key.cycle(2, 'enter')
+
+    #Заполнение формы
+    time.sleep(3)
+    text.field_paste(name)
+    pyautogui.press('tab')
+    text.field_paste(name[0])
+    text.field_paste(patronymic[0])
+    pyautogui.press('tab')
+    text.field_paste(surname)
+    pyautogui.press('tab')
+    pyautogui.press('del')
+    text.field_paste(surname)
+    text.field_paste(' ')
+    text.field_paste(name)
+    text.field_paste(' ')
+    text.field_paste(patronymic)
+    pyautogui.press('tab')
+    text.field_paste(login)
+    key.cycle(4, 'tab')
+    pyautogui.press('enter')
+    text.field_paste(password)
+    pyautogui.press('tab')
+    text.field_paste(password)
+    key.cycle(7, 'tab')
+    key.cycle(6, 'enter')
+    pyperclip.copy('')
+
+    #Ожидание закрытия окна
+    while True:
+        try:
+            pyautogui.getWindowsWithTitle("Новый объект - Пользователь")[0]
+        except:
+            break
